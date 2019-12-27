7e1a564c0ca0ea868eae42e02a36e83372d0d654
Askar Karasaev <askar.karasaev@gmail.com>
2019-12-27 07:28:43 +0000

    Adds the user create endpoint

--- test/controllers/api/v1/users_controller_test.rb
+++ test/controllers/api/v1/users_controller_test.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
@@ -11,5 +13,20 @@ class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
     # Test to ensure response contains the correct email
     json_response = JSON.parse(response.body)
     assert_equal @user.email, json_response['email']
+  end
+
+  test 'should create a user' do
+    assert_difference('User.count') do
+      post api_v1_users_url,
+           params: {user: { email: 'test@test.org', password: '123456' }}, as: :json
+    end
+    assert_response :created
+  end
+
+  test 'should not create a user with already registered email' do
+    assert_no_difference('User.count') do
+      post api_v1_users_url, params: { user: { email: @user.email, password: '123456' }}, as: :json
+    end
+    assert_response :unprocessable_entity
   end
 end
